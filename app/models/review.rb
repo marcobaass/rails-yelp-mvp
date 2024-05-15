c7a60f7ab326b032d9daa5a2cdab71e390e078a2
Marco Baass <picture@marcobaass.com>
2024-05-15 17:12:41 +0200

    restaurants part 1

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, comparison: { greater_than_or_equal_to: 0 }
+  validates :rating, comparison: { less_than_or_equal_to: 5 }
+  validates :rating, numericality: { only_integer: true }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+inizio = { name: "Inizio Ristorante Bar", address: "Fasanenstraße 77, 10623 Berlin Deutschland", phone_number: "+49 30 959999131", category: "italian" }
+buschbeck = { name: "Restaurant Buschbeck's", address: "Trendelenburgstr. 14, 14057 Berlin Deutschland", phone_number: "+49 30 32604166", category: "belgian" }
+focaccino = { name: "Focaccino", address: "Stuttgarter Platz 16 Charlottenburg,Wilmesdorf, 10627 Berlin Deutschland", phone_number: "+49 30 3248838", category: "italian" }
+hackethals = { name: "Hackethals", address: "Pflugstrasse 11, 10115 Berlin Deutschland", phone_number: "+49 30 28387765", category: "belgian" }
+cafe_couscous = { name: "Café Couscous", address: "Friedrichstr. 115, 10117 Berlin Deutschland", phone_number: "+49 30 55070382", category: "french" }
+
+[inizio, buschbeck, focaccino, hackethals, cafe_couscous].each do |attributes|
+  rate = rand(0..5)
+  restaurant = Restaurant.create!(attributes)
+  reviews = Review.create!(content: "super", rating: rate, restaurant_id: restaurant.id)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
+# inizio = { name: "Inizio Ristorante Bar", address: "Fasanenstraße 77, 10623 Berlin Deutschland", rating: 4 }
+# buschbeck = { name: "Restaurant Buschbeck's", address: "Trendelenburgstr. 14, 14057 Berlin Deutschland", rating: 5 }
+# focaccino = { name: "Focaccino", address: "Stuttgarter Platz 16 Charlottenburg,Wilmesdorf, 10627 Berlin Deutschland",
+# rating: 4 }
+# hackethals = { name: "Hackethals", address: "Pflugstrasse 11, 10115 Berlin Deutschland", rating: 4 }
+# cafe_couscous = { name: "Café Couscous", address: "Friedrichstr. 115, 10117 Berlin Deutschland", rating: 5 }
